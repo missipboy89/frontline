bd866a3fdb54424541ba784b565355215996484b
missipboy89 <missipboy89@gmail.com>
2020-10-01 21:50:01 -0500

    Layout

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,15 @@
 <template>
   <div>
-
-    <div class="ui inverted segment">
-      <div class="ui inverted secondary menu">
-        <a href="/" class="red item active">Home</a>
-        <a href="/" class="red item active">Mission</a>
-        <a href="/" class="red item active">Calendar</a>
-        <a href="/" class="red item active">Contact</a>
-        <div class="right menu">
-          <a href="/" class="red item active">Login</a>
-        </div>
-      </div>
-    </div>
-    <h1 class="platoon">
-        FRONTLINE SOC
-    </h1>
-
+    <Layout></Layout>
   </div>
 </template>
 
 <script>
+import Layout from './components/Layout';
 export default {
   name: 'App',
+  components: {
+    Layout
+  }
 }
 </script>
-
-<style>
-@font-face {
-  font-family: PLATOoN;
-  src: local("PLATOoN"), 
-  url(./fonts/PLATOoN.ttf) format("truetype");
-}
-
-.platoon {
-  font-family: "PLATOoN";
-  font-size: 120px;
-  color: solid black;
-  text-align: center;
-}
-</style>
