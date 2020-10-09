a7fd8b06e1b32217e5a6a17f66ffc215c77a4863
missipboy89 <missipboy89@gmail.com>
2020-10-08 22:10:15 -0500

    changes

--- src/components/BiblePassage.vue
+++ src/components/BiblePassage.vue
@@ -1,19 +1,14 @@
 <template>
     <div>
-        <form action="https://www.biblegateway.com/quicksearch/">
-<table border="1" cellspacing="0" cellpadding="2" style="border-color: #600;">
-<tr><td style="background-color: lightgrey; text-align: center; padding: 0 10px;">
-<div style="margin-bottom: 0;">
-<input style="margin:5px" type="text" name="quicksearch" /><br />
-<input style="margin:5px" type="submit" value="Search BibleGateway.com" /><br />
-</div>
-<a href="https://www.biblegateway.com/" title="The Bible in multiple languages, versions, and formats">
-</a><br />
-<small>
-</small>
-</td></tr>
-</table>
-</form>
+        <div class="ui inverted segment">
+            <form action="https://www.biblegateway.com/quicksearch/" target="_blank">
+            <div class="ui fluid icon input"><input @input="onInput" type="text" placeholder="Search..." name="quicksearch">
+            <button class="ui button">Search Bible Gateway</button>
+            <a href="https://www.biblegateway.com/" title="The Bible in multiple languages, versions, and formats" >
+            </a>
+            </div>
+            </form>
+        </div>
     </div>
 </template>
 
